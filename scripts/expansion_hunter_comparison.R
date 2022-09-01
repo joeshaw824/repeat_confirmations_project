ff783652cdc9326e124685106e428428c1d4cf20
Joseph Shaw <Joseph.Shaw@gosh.nhs.uk>
2022-09-01 17:13:19 +0100

    Addition of Expansion Hunter comparison script.

---
+++ scripts/expansion_hunter_comparison.R
@@ -0,0 +1,55 @@
+################################################################################
+## Comparison of ExpansionHunter and Orthogonal Testing
+################################################################################
+
+##############################
+# Load libraries
+##############################
+
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+setwd(dir = "W:/MolecularGenetics/Neurogenetics/Research/Joe Shaw Translational Post 2022/RFC1 R code/RFC1_analysis")
+
+##############################
+# Load data
+##############################
+
+comparison_data <- read_excel(path = "data/combined_othogonal_comparison.xlsx",
+                              sheet = "combined_othogonal_output") %>%
+  janitor::clean_names()
+
+
+##############################
+# Plot data
+##############################
+
+allele_1_results <- comparison_data %>%
+  select(-c(pcr_allele_size_2, eh_min_ci_allele_2, eh_allele_2, eh_max_ci_allele_2, 
+            pcr_eh_max_ci_allele_2, pcr_eh_max_ci_allele_1, investigation)) %>%
+  dplyr::rename(pcr_allele_size = pcr_allele_size_1,
+                eh_min_ci = eh_min_ci_allele_1,
+                eh_allele = eh_allele_1,
+                eh_max_ci = eh_max_ci_allele_1)
+
+allele_2_results <- comparison_data %>%
+  select(-c(pcr_allele_size_1, eh_min_ci_allele_1, eh_allele_1, eh_max_ci_allele_1, 
+            pcr_eh_max_ci_allele_2, pcr_eh_max_ci_allele_1, investigation)) %>%
+  dplyr::rename(pcr_allele_size = pcr_allele_size_2,
+                eh_min_ci = eh_min_ci_allele_2,
+                eh_allele = eh_allele_2,
+                eh_max_ci = eh_max_ci_allele_2)
+
+longer_results <- rbind(allele_1_results, allele_2_results)
+
+longer_results %>%
+  ggplot(aes(x = pcr_allele_size, y = eh_allele)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = eh_min_ci, ymax = eh_max_ci)) +
+  geom_abline(linetype="dashed") +
+  ylim(0, 50) +
+  xlim(0, 50) +
+  facet_wrap(~repeat_id)
+
+##############################
--- scripts/rfc1_dna_aliquots.R
+++ scripts/rfc1_dna_aliquots.R
@@ -20,16 +20,8 @@ setwd(dir = "W:/MolecularGenetics/Neurogenetics/Research/Joe Shaw Translational 
 # Extracts all samples with an RFC1 test set added
 
 rfc1_epic_requests <- read_excel(path = "data/CANVAS_Referrals_with_Clinicians_20220831_1507.xlsx") %>%
-  janitor::clean_names()
-
-july_referrals <- grep("2022-07", rfc1_epic_requests$collection_instant, value = TRUE)
-
-rfc1_epic_requests %>% 
-  filter(substr(collection_instant, 1, 7) == "2022-07")
-
-# 27 samples per month
-# By December 2022 there will be 455 + (27*4) = 563 samples for testing
-
+  janitor::clean_names() %>%
+  dplyr::rename(specimen_id = test_specimen_id)
 
 # Roy's spreadsheet of sample details he sent to Andrea
 sent_samples <- read_excel("W:/MolecularGenetics/Neurogenetics/Research/CANVAS/Canvas list sent to Andrea/Combined_Beaker-EPIC_CANVAS_list_sent.xlsx") %>%
@@ -42,13 +34,16 @@ sent_samples <- read_excel("W:/MolecularGenetics/Neurogenetics/Research/CANVAS/C
 # This is an Excel that I manually typed out from the paper forms in the prep lab research
 # aliquots folder.
 # These samples were aliquotted by the GOSH prep lab team and sent to the Cortese group.
-prep_pab_samples <- read_excel("data/samples_requested_by_cortese_group.xlsx",
+prep_lab_samples <- read_excel("data/samples_requested_by_cortese_group.xlsx",
                                sheet = "aliquots") %>%
   dplyr::rename(specimen_id = episode)
 
-aliquotted_prep_lab_samples <- prep_pab_samples %>%
+aliquotted_prep_lab_samples <- prep_lab_samples %>%
   # Remove samples where no sample was sent
-  filter(volume_ul != 0)
+  filter(volume_ul != 0) %>%
+  left_join(rfc1_epic_requests %>%
+              select(specimen_id, pt_first_nm, patient_surname, dob),
+            by = "specimen_id")
 
 # This is Mark Gaskin's pull sheet for DNA samples that he aliquotted on 11/08/2022
 gaskin_samples <- read_excel(path = "W:/MolecularGenetics/Neurogenetics/Research/Joe Shaw Translational Post 2022/DNA_aliquots_for_research/T1611-Pull sheet.xlsx",
@@ -61,7 +56,18 @@ gaskin_samples <- read_excel(path = "W:/MolecularGenetics/Neurogenetics/Research
 
 aliquotted_gaskin_samples <- gaskin_samples %>%
   # Remove samples which were "missing" or "empty"
-  filter(sample_status == "Present")
+  filter(sample_status == "Present") %>%
+  left_join(rfc1_epic_requests %>%
+              select(specimen_id, pt_first_nm, patient_surname, dob),
+            by = "specimen_id")
+
+all_aliquotted_samples
+
+colnames(prep_lab_samples)
+
+gaskin_samples 
+
+date_aliquotted == "2022-08-11"
 
 ##############################
 # Which samples haven't been aliquotted?
@@ -69,10 +75,15 @@ aliquotted_gaskin_samples <- gaskin_samples %>%
 
 aliquotted_samples <- c(aliquotted_gaskin_samples$specimen_id, aliquotted_prep_lab_samples$specimen_id)
 
-not_aliquotted <- setdiff(rfc1_epic_requests$test_specimen_id, aliquotted_samples)
+not_aliquotted <- setdiff(rfc1_epic_requests$specimen_id, aliquotted_samples)
 
 not_aliquotted_details <- rfc1_epic_requests %>%
-  filter(test_specimen_id %in% not_aliquotted) %>%
+  filter(specimen_id %in% not_aliquotted) %>%
+  select(-c("submitter_ordering_dept", "external_requisition_comment", "storage_location",
+            "final_dna_conc_ng_ml", "batch"))
+
+`aliquotted_details <- rfc1_epic_requests %>%
+  filter(!test_specimen_id %in% not_aliquotted) %>%
   select(-c("submitter_ordering_dept", "external_requisition_comment", "storage_location",
             "final_dna_conc_ng_ml", "batch"))
 
@@ -83,5 +94,12 @@ not_on_spreadsheet_samples <- rfc1_epic_requests %>%
   select(-c("submitter_ordering_dept", "external_requisition_comment", "storage_location",
             "final_dna_conc_ng_ml", "batch"))
 
+july_referrals <- grep("2022-07", rfc1_epic_requests$collection_instant, value = TRUE)
+
+august_referrals <- grep("2022-08", rfc1_epic_requests$collection_instant, value = TRUE)
+
+to_check <- not_aliquotted_details %>% 
+  filter(substr(collection_instant, 1, 7) != "2022-07" &
+           substr(collection_instant, 1, 7) != "2022-08")
 
 ##############################
